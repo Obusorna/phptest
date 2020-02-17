e4af44f3986183f274c53758ff18110ce04370d1
Obusorna <60769065+Obusorna@users.noreply.github.com>
2020-02-17 15:36:13 +0530

    Update Test.php

--- Phptest/src/PHP/Test.php
+++ Phptest/src/PHP/Test.php
@@ -1,1 +1,14 @@
+<?php
+require_once('include/init.phph');
+use RightNow\Connect\v1_3 as RNCPHP;
+require_once( get_cfg_var("doc_root")."/ConnectPHP/Connect_init.php" );
+initConnetAPI();
+
+try{
+  
+  
+
 foo();
+
+  
+}
