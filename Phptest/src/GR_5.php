a3dfe419ca97fd4dfc138c4215b925e338c0c6bb
Obusorna <60769065+Obusorna@users.noreply.github.com>
2020-03-06 16:54:15 +0530

    Add files via upload

---
+++ Phptest/src/GR_5.php
@@ -0,0 +1,43 @@
+<?
+
+ 
+
+require_once('include/init.phph');
+
+require_once(get_cfg_var("doc_root") . "/ConnectPHP/Connect_init.php");
+
+use RightNow\Connect\v1_2 as RNCPHP;
+initConnectAPI(); 
+
+ 
+
+
+try{
+$result = RNCPHP\ROQL::queryObject("SELECT Contact FROM Contact WHERE Contact.CustomFields.c.memberid='$memberid'")->next();
+
+
+foo();
+bar();
+//$first_name = $row->Name->First;
+
+while ($row = $result->next())
+ 
+{
+    $contact_id = $row->ID;
+  
+  $first_name = $row->Name->First;
+   
+ $last_name = $row->Name->Last;
+    
+}
+}
+
+
+catch(Exception $e)
+
+{
+    print_r($e->getMessage());
+}
+
+
+?>
